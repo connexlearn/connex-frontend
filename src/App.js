33ab6bc712d687e3b1005742f6cc040c95e34c7b
Connex <mohanpandit131@gmail.com>
2022-08-06 15:27:33 +0530

    nav

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <h1>hello i am anish</h1>
-       <p>hello you are mohan pandit</p>
-      </header>
-    </div>
+  <>
+  
+  
+  </>
   );
 }
 
